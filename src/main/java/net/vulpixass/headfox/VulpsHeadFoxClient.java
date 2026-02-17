110f3c4b28849b0ddc655f1e2b3509a30aa36bde
Vulpixass <maxderfuchs165@gmail.de>
2026-02-17 20:13:51 +0100

    Initial Commit

---
+++ src/main/java/net/vulpixass/headfox/VulpsHeadFoxClient.java
@@ -0,0 +1,22 @@
+package net.vulpixass.headfox;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.LivingEntityFeatureRendererRegistrationCallback;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.minecraft.entity.EntityType;
+import net.vulpixass.headfox.client.HeadFoxModelLayers;
+import net.vulpixass.headfox.client.model.fox_baby;
+import net.vulpixass.headfox.client.render.FoxHatFeatureRenderer;
+
+public class VulpsHeadFoxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        EntityModelLayerRegistry.registerModelLayer(HeadFoxModelLayers.FOX_LAYER, fox_baby::getTexturedModelData );
+        LivingEntityFeatureRendererRegistrationCallback.EVENT.register((entityType, renderer, registrationHelper, ctx) -> {
+            if (entityType == EntityType.PLAYER && renderer instanceof PlayerEntityRenderer playerRenderer) {
+                registrationHelper.register(new FoxHatFeatureRenderer(playerRenderer));
+            }
+        });
+    }
+}
